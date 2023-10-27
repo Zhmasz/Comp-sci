948914dd981e42cd12300053e989d033872f2c8c
Zhmas <146401166+Zhmasz@users.noreply.github.com>
2023-10-27 09:48:07 +0800

    Add files via upload

---
+++ Pr21.java
@@ -0,0 +1,12 @@
+public class Pr21 {
+	
+	public static void main (String[] args)
+	 {
+		int n = IBIO.inputInt("input a number between 50 and 60 = ");
+		if (n>60)
+		IBIO.output("that number was too big");
+		else if (n<50)
+		IBIO.output("that number was too small");
+	}
+}
+
