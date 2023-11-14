e4b622ca8cd4ceedff74a663c8706b7c3fdf9c82
Zhmas <146401166+Zhmasz@users.noreply.github.com>
2023-11-14 10:44:07 +0800

    Pr43.java

--- Pr43.java
+++ Pr43.java
@@ -1,26 +1,26 @@
 public class Pr43 {
 	
-	public static void main (String[] args)
-	 {
-		int n;
-		int search;
-		do{
-			n=IBIO.inputInt("Enter a number within 1000: ");
-		if(n>=1000){
-			System.out.println("Number too large!");
+	public static void main (String[] args) {
+	int n;
+	int sum;
+	boolean finding=false;
+	do{
+		n = IBIO.inputInt("Enter a number less than 1000: ");
+	}while(n>=1000);
+	
+	for (int x=0;x<2;x++){
+		for (int y=0;y<1000;y++){
+			sum=x*x + y*y;
+		if (sum==n){
+		System.out.println("The square of "+x+" and "+y+" makes "+n);
+		finding=true;
+		break;
 		}
-		}while(n>1000);
-
-		for (int x=0;x<=2;x++){
-			for (int y=0;y<=1000;y++){
-				search=x*x + y*y;
-				if (search==n){
-				System.out.println(x + ", " + y);
-				}
-			}
-		}
-		System.out.println("Impossible");
+	}
+	}
+	if(!finding){
+		System.out.println("Impossible!");
 	}
 }
-
+}
 
