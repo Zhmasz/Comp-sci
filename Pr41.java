eec7e1b544c039c3b7f6ce9860f293d1ec837245
Zhmas <146401166+Zhmasz@users.noreply.github.com>
2023-11-14 10:23:49 +0800

    Pr41.java

--- Pr41.java
+++ Pr41.java
@@ -1,23 +1,28 @@
 public class Pr41 {
 	
-	public static void main (String[] args)
-	 {
-		int x=IBIO.inputInt("Enter a number: ");
-		int y=IBIO.inputInt("Enter another number: ");
-		int action=IBIO.inputInt("\nEnter:\n[1] for add \n[2] for multiply \n[3] for quit\n");
-		int z;
-		switch(action){
+	public static void main (String[] args) {
+		int a=IBIO.inputInt("Enter a number: ");
+		int b=IBIO.inputInt("Enter another number: ");
+		int c;
+	do{
+		c=IBIO.inputInt("Click on [1] for addition [2] for multiplication [3] for quit\n");
+		switch(c){
 			case 1:
-				z=x+y;
-				System.out.println("The result of "+x+" plus "+y+" is "+ z);
-				break;
+			System.out.println();
+			System.out.println(a+" plus "+b+" is equal to "+ (a+b));
+			break;
 			case 2:
-				System.out.println("The result of "+x+" times "+y+" is "+ x*y);
-				break;
+			System.out.println();
+			System.out.println(a+" times "+b+" is equal to "+ (a*b));
+			break;
 			case 3:
-				System.exit(0);
-				break;
+			System.out.println("Quitting!");
+			System.exit(0);
 		}
+		if(c>=3){
+			System.out.print("Error, ");
+		}
+	}while(c>=3);	
 	}
 }
 
